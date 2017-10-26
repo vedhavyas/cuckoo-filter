e1b6c7184f7a93eff358bf393cbf3b023a271779
vedhavyas <vedhavyas.singareddi@gmail.com>
2017-10-26 14:03:19 +0530

    refactor tests

--- cuckoo_test.go
+++ cuckoo_test.go
@@ -1,6 +1,9 @@
 package cuckoo
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strconv"
 	"testing"
 
@@ -225,4 +228,114 @@ func Test_nextPowerOf2(t *testing.T) {
 			t.Fatalf("expected %d but got %d", c.e, g)
 		}
 	}
+}
+
+// Benchmark tests taken from https://github.com/mtchavez/cuckoo
+var filter *Filter
+var okay bool
+
+func BenchmarkCuckooNew(b *testing.B) {
+	b.ReportAllocs()
+	var f *Filter
+	for i := 0; i < b.N; i++ {
+		f = StdFilter()
+	}
+
+	filter = f
+}
+
+func BenchmarkInsert(b *testing.B) {
+	var ok bool
+	filter := StdFilter()
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ok = filter.Insert([]byte(fmt.Sprintf("item-%d", i%50000)))
+	}
+
+	okay = ok
+}
+
+func BenchmarkInsertUnique(b *testing.B) {
+	var ok bool
+	filter := StdFilter()
+	fd, _ := os.Open("/usr/share/dict/words")
+	defer fd.Close()
+
+	scanner := bufio.NewScanner(fd)
+	var wordCount int
+	var totalWords int
+	var values [][]byte
+	for scanner.Scan() {
+		word := []byte(scanner.Text())
+		totalWords++
+
+		if filter.Insert(word) {
+			wordCount++
+		}
+		values = append(values, word)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ok = filter.InsertUnique(values[i%totalWords])
+	}
+
+	okay = ok
+}
+
+func BenchmarkLookup(b *testing.B) {
+	var ok bool
+	filter := StdFilter()
+	fd, _ := os.Open("/usr/share/dict/words")
+	defer fd.Close()
+
+	scanner := bufio.NewScanner(fd)
+	var wordCount int
+	var totalWords int
+	var values [][]byte
+	for scanner.Scan() {
+		word := []byte(scanner.Text())
+		totalWords++
+
+		if filter.Insert(word) {
+			wordCount++
+		}
+		values = append(values, word)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ok = filter.Lookup(values[i%totalWords])
+	}
+
+	okay = ok
+}
+
+func BenchmarkDelete(b *testing.B) {
+	var ok bool
+	filter := StdFilter()
+	fd, _ := os.Open("/usr/share/dict/words")
+	defer fd.Close()
+
+	scanner := bufio.NewScanner(fd)
+	var wordCount int
+	var totalWords int
+	var values [][]byte
+	for scanner.Scan() {
+		word := []byte(scanner.Text())
+		totalWords++
+
+		if filter.Insert(word) {
+			wordCount++
+		}
+		values = append(values, word)
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ok = filter.Delete(values[i%totalWords])
+	}
+
+	okay = ok
 }
